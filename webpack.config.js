7eeea3d2ad9510991e4c74c4f942cce40441a6df
chrisbolosan <cbolosan@gmail.com>
2022-01-27 11:34:58 -0600

    dummy data

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.tsx",
+  devtool: "eval-source-map",
   resolve: {
     extensions: [".js", ".ts", ".tsx"],
   },
