f1f1206208683c84b708abf4eeabee5cbb5ce20f
chrisbolosan <cbolosan@gmail.com>
2022-01-30 14:14:13 -0600

    refactor with new prop component

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import PizzaCSS from "./Pizza.module.css";
 import { Pizza } from "../types";
-import { withAddToCart, AddToCartProps } from "./AddToCart";
+import { WithAddToCartProps } from "./AddToCart";
 
-interface Props extends AddToCartProps {
+interface Props {
   pizza: Pizza;
 }
 
-const PizzaItem: React.FC<Props> = ({ pizza, addToCart }) => {
-  const handleAddToCart = () => {
-    addToCart({
-      id: pizza.id,
-      name: pizza.name,
-      price: pizza.price,
-    });
-  };
+const PizzaItem: React.FC<Props> = ({ pizza }) => {
   return (
     <li className={PizzaCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <button type="button" onClick={handleAddToCart}>
-        Add to Cart
-      </button>
+      <WithAddToCartProps>
+        {({ addToCart }) => {
+          return (
+            <button
+              type="button"
+              onClick={() =>
+                addToCart({
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                })
+              }
+            >
+              Add to Cart
+            </button>
+          );
+        }}
+      </WithAddToCartProps>
     </li>
   );
 };
 
-export default withAddToCart(PizzaItem);
+export default PizzaItem;
