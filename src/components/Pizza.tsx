f0b0119cde6300567ed0ca82e8b3a27d71711af9
chrisbolosan <cbolosan@gmail.com>
2022-01-30 14:26:49 -0600

    custom hook work

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,36 +1,29 @@
 import React from "react";
 import PizzaCSS from "./Pizza.module.css";
 import { Pizza } from "../types";
-import { WithAddToCartProps } from "./AddToCart";
+import { useAddToCart } from "./AddToCart";
 
 interface Props {
   pizza: Pizza;
 }
 
 const PizzaItem: React.FC<Props> = ({ pizza }) => {
+  const addToCart = useAddToCart();
+  const handleAddToCart = () => {
+    addToCart({
+      id: pizza.id,
+      name: pizza.name,
+      price: pizza.price,
+    });
+  };
   return (
     <li className={PizzaCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <WithAddToCartProps>
-        {({ addToCart }) => {
-          return (
-            <button
-              type="button"
-              onClick={() =>
-                addToCart({
-                  id: pizza.id,
-                  name: pizza.name,
-                  price: pizza.price,
-                })
-              }
-            >
-              Add to Cart
-            </button>
-          );
-        }}
-      </WithAddToCartProps>
+      <button type="button" onClick={handleAddToCart}>
+        Add to Cart
+      </button>
     </li>
   );
 };
