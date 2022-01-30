f440c82689a6cc58f428ac76006e8e1bb66c026a
chrisbolosan <cbolosan@gmail.com>
2022-01-30 14:21:46 -0600

    render prop in special offer

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -43,3 +43,16 @@ export const WithAddToCartProps: React.FC<{
   };
   return children({ addToCart });
 };
+
+export const useAddToCart = () => {
+  const dispatch = useStateDispatch();
+  const addToCart: AddToCartProps["addToCart"] = (item) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        item,
+      },
+    });
+  };
+  return addToCart;
+};
--- src/components/SpecialOffer.tsx
+++ src/components/SpecialOffer.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { Pizza } from "../types";
 import SpecialOfferCSS from "./SpecialOffer.module.css";
-import { withAddToCart, AddToCartProps } from "./AddToCart";
+import { WithAddToCartProps } from "./AddToCart";
 
-interface Props extends AddToCartProps {
+interface Props {
   pizza: Pizza;
 }
 
-const SpecialOffer: React.FC<Props> = ({ pizza, addToCart }) => {
-  const handleAddToCart = () => {
-    addToCart({
-      id: pizza.id,
-      name: pizza.name,
-      price: pizza.price,
-    });
-  };
+const SpecialOffer: React.FC<Props> = ({ pizza }) => {
   return (
     <div className={SpecialOfferCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <button type="button" onClick={handleAddToCart}>
-        Add to Cart
-      </button>
+      <WithAddToCartProps>
+        {({ addToCart }) => {
+          return (
+            <button
+              type="button"
+              onClick={() =>
+                addToCart({
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                })
+              }
+            >
+              Add to Cart
+            </button>
+          );
+        }}
+      </WithAddToCartProps>
     </div>
   );
 };
 
-export default withAddToCart(SpecialOffer);
+export default SpecialOffer;
